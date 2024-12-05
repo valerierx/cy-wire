3bc99ca2fb68eec5cbe95601bfb6348d61cc7bb3
BifidusMagnus <168721086+BifidusMagnus@users.noreply.github.com>
2024-12-05 11:10:09 +0100

    feat: ajout cy-wire.sh

---
+++ cy-wire.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+
+fichier=$1
+station=$2
+client=$3
+
+
+if [ ! -f $fichier ] #assertion
+then
+
+    echo "Erreur : Le premier argument n'est pas un fichier ou il n'existe pas"
+    exit 0
+
+elif [[ ! $fichier = *.csv ]] #Double crochet nécessaire pour comparer avec le metacraractere *
+then
+
+    echo "Erreur fichier au mauvais format"
+    exit 0
+
+fi
+
+
+
+case $station in  #Identification de la station
+
+    hvb)
+
+        echo "ça marche, étudion les hvb"
+
+        if [ "$client" = "comp" ] # Vérification option
+        then
+            echo "Option hvb entreprise -> suite en c."
+                
+        else
+
+            echo "Erreur Option impossible avec les hvb, seul les entreprise peuvent être raccorder au hvb."
+            exit 2
+
+        fi
+
+    ;;
+
+    hva)
+
+        echo "ça marche, étudion les hva"
+
+        if [ "$client" = "comp" ] # Vérification option
+        then
+            echo "Option hva entreprise -> suite en c."
+                
+        else  
+
+            echo "Erreur Option impossible avec les hva, seul les entreprise peuvent être raccorder au hva."
+            exit 2
+
+        fi
+
+
+    ;;
+
+    lv)
+
+        echo "ça marche, étudions les lv"
+
+        case $client in # Vérification option
+
+            comp)
+
+                echo "lv -> comp"
+
+            ;;
+
+            indiv)
+
+                echo "lv -> indiv"
+
+            ;;
+
+            all)
+
+                echo " lv -> all"
+
+            ;;
+
+            *)
+
+                echo "Erreur Option lv, option voulu : comp, indiv, all."
+                exit 4
+
+            ;;
+
+            esac
+
+    ;;
+
+    *)
+
+        echo "Erreur : l'option de station est incorrecte"
+        exit 1
+
+    ;;
+
+esac
+
+
+
+
+
+
