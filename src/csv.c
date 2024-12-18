f6621e25908fd420284560258b2a85c222e5e5b7
yaniskss91 <167715043+yaniskss91@users.noreply.github.com>
2024-12-18 14:03:35 +0100

    modif

--- src/csv.c
+++ src/csv.c
@@ -14,6 +14,7 @@ Station* lecture_fichier(Station * fstation){
     long long consommation=0;
     char ligne[256];
     int h=0; //Equilibrage AVL;
+    int surconsommation;
 
     if(fichier==NULL){
         printf("Impossible d'ouvrir le fichier\n");
@@ -26,10 +27,15 @@ Station* lecture_fichier(Station * fstation){
         if(noeud!=NULL){
             noeud->capacite=noeud->capacite+capacite;
             noeud->consommation=noeud->consommation+consommation;
+            if((noeud->capacite - noeud->consommation)<0){
+                surconsommation=-(noeud->capacite - noeud->consommation)
+                printf("La station %d est en surconsommation de %d ",id,surconsommation);
+            }
         } else {
             fstation=insererStation(fstation,id,capacite,consommation,&h);
         }
     }
+    
     fclose(fichier);
     return fstation;
 }
