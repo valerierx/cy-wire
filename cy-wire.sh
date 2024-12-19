c680c7236ad013d5fd1bf6950f943dee9ea3b6fb
BifidusMagnus <168721086+BifidusMagnus@users.noreply.github.com>
2024-12-19 10:19:46 +0100

    feat : ajout cy-wire : grep, traitement des power plant, ajout du dossier output, chronometrage des traitement

--- cy-wire.sh
+++ cy-wire.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/bash
 
 set -e # le programme se termine si l'une des commandes retourne une erreur
@@ -172,3 +173,182 @@ let m=$(($Chrono / 60))
 let s=$(($Chrono % 60))
 
 echo "Temps de traitement : $m minutes $s secondes"
+=======
+#!/bin/bash
+
+set -e # le programme se termine si l'une des commandes retourne une erreur
+
+if [ ! -f cy-wire ]; then
+    echo "Avertissement: l'exécutable cy-wire n'a pas été trouvé, lancement de la compilation"
+    make -j$(nproc)
+fi
+
+for i in $@
+        do
+	    if [ "$i" = "-h" ]
+        then
+            cat README.md
+            exit 0
+        fi
+
+done
+
+
+fichier=$1
+station=$2
+client=$3
+power=$4
+
+
+if [ ! -f $fichier ] #assertion
+then
+
+    echo "Erreur : Le premier argument n'est pas un fichier ou il n'existe pas"
+    cat README.md
+    exit 0
+
+elif [[ ! $fichier = *.csv && ! $fichier = *.dat ]] #Double crochet nécessaire pour comparer avec le metacraractere *
+then
+
+    echo "Erreur fichier au mauvais format"
+    cat README.md
+    exit 0
+
+fi
+
+
+if [ "$power" = "all" ]
+    then
+        power="^[0-9]+"
+        extension=""
+    
+elif [ $power -ge 6 ]
+    then
+        echo "Erreur : option de power plant"
+        cat README.txt 
+        exit 0
+else
+        extention="_$power"
+fi
+
+
+
+if [  -d "tmp" ]
+then
+
+    echo "Le dossier tmp existe, suppression de son contenu."
+    rm tmp/*
+
+else
+
+    echo "Le dossier tmp existe pas, création du dossier"
+    mkdir tmp
+
+fi
+
+
+
+chrono1=`date +%s` #début du chronometrage du traitement
+
+
+case $station in  #Identification de la station
+
+     hvb)
+
+
+        if [ "$client" = "comp" ] # Vérification option
+        then
+
+            echo "Option hvb entreprise."
+
+            grep -E "$power;.*;.*;.*;.*;.*;.*;.*$" $fichier | cut -d';' -f2,3,5,7,8 | grep -E "^[0-9]+;-;.*;.*$" | cut -d';' -f1,4,5 | tr "-" "0" > tmp/data.txt 
+
+            #c
+
+
+        else
+
+            echo "Erreur Option impossible avec les hvb"
+            cat README.md
+            exit 2
+
+        fi
+
+    ;;
+
+    hva)
+
+        if [ "$client" = "comp" ] # Vérification option
+        then
+
+            echo "Option hva entreprise"
+
+            grep -E "$power;.*;.*;.*;.*;.*;.*;.*$" $fichier | cut -d';' -f3,4,5,7,8 | grep -E "^[0-9]+;-;.*;.*;.*$" | cut -d';' -f1,4,5 | tr "-" "0" > tmp/data.txt
+
+        else
+
+            echo "Erreur Option impossible avec les hva"
+            cat README.md
+            exit 2
+
+        fi
+
+
+    ;;
+
+    lv)
+
+        case $client in # Vérification option
+
+            comp)
+
+                echo "lv -> comp"
+                grep -E "$power;.*;.*;.*;.*;.*;.*;.*$" $fichier | cut -d';' -f4,5,6,7,8 | grep -E "^[0-9]+;.*;-;.*;.*$" | cut -d';' -f1,4,5 | tr "-" "0" > tmp/data.txt
+
+            ;;
+
+            indiv)
+
+                echo "lv -> indiv"
+                grep -E "$power;.*;.*;.*;.*;.*;.*;.*$" $fichier | cut -d';' -f4,5,6,7,8 | grep -E "^[0-9]+;-;.*$" | cut -d';' -f1,4,5 | tr "-" "0" > tmp/data.txt
+
+            ;;
+
+            all)
+
+                 echo " lv -> all"
+                 grep -E "$power;.*;.*;.*;.*;.*;.*;.*$" $fichier | cut -d';' -f4,5,6,7,8 | grep -E "^[0-9]+.*$" | cut -d';' -f1,4,5 | tr "-" "0" > tmp/data.txt
+
+            ;;
+
+            *)
+
+                echo "Erreur Option lv"
+                cat README.md
+                exit 4
+
+            ;;
+
+            esac
+
+    ;;
+
+    *)
+
+        echo "Erreur : l'option de station est incorrecte"
+        cat README.md
+        exit 1
+
+    ;;
+
+esac
+
+
+chrono2=`date +%s`
+
+let Chrono=$(($chrono2 - $chrono1))
+let m=$(($Chrono / 60))
+let s=$(($Chrono % 60))
+
+echo "Temps de traitement : $m minutes $s secondes"
+>>>>>>> 292b7c3 (feat : ajout cy-wire : grep, traitement des power plant, ajout du dossier output, chronometrage des traitement)
